a5514c9274635c4a765b94e97889c17f4c345ffe
oussama <znagui.info@gmail.com>
2024-09-19 15:02:48 +0100

    BAT-2 Client model

---
+++ out/production/BatiCuisine-Java-App/main/java/ma/znagui/DataBase/main.sql
@@ -0,0 +1,68 @@
+#bonjour --
+CREATE TYPE ProjectStatus AS ENUM('INPROGRESS','COMPLETED','CANCELLED');
+CREATE TYPE ComponentType AS ENUM('LABOR','EQUIPMENT');
+
+create table Clients(
+    id SERIAL PRIMARY KEY,
+    name varchar,
+    email varchar,
+    phone varchar,
+    isPro boolean
+);
+
+create table Projects(
+    id SERIAL PRIMARY KEY,
+    projectName varchar,
+    profitMargin decimal,
+    totalCost decimal,
+    projectStatus ProjectStatus,
+    clientID int
+);
+
+create table Estimates(
+   id SERIAL PRIMARY KEY,
+   estimatedAmount decimal,
+   issueDate decimal,
+   expirationDate decimal,
+   accepted boolean,
+    projectID int,
+
+);
+
+
+create table components(
+    id SERIAL PRIMARY KEY,
+    name varchar,
+    type ComponentType,
+    tva int,
+    projectID int
+);
+
+create table Labors(
+    hourlyRate decimal,
+    workingHours decimal,
+    productivityCff decimal
+) INHERITS (components);
+
+create table Equipments(
+    unitCost decimal,
+    quantity decimal,
+    transportCost decimal,
+    qualityCoefficient decimal
+) INHERITS (components);
+
+ALTER TABLE Projects
+    ADD FOREIGN KEY (clientID) REFERENCES Clients(id);
+ALTER TABLE Estimates
+    ADD FOREIGN KEY (projectID) REFERENCES Projects(id);
+ALTER TABLE components
+    ADD FOREIGN KEY (projectID) REFERENCES Projects(id);
+
+
+
+
+
+
+
+
+
