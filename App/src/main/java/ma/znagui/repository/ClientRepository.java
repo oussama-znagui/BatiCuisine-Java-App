8c6b1def955ad18178931de42b3602f1f6c8bf7b
oussama <znagui.info@gmail.com>
2024-09-20 15:30:15 +0100

    BAT-2 Client Repository Crud

--- App/src/main/java/ma/znagui/repository/ClientRepository.java
+++ App/src/main/java/ma/znagui/repository/ClientRepository.java
@@ -1,6 +1,8 @@
 package ma.znagui.repository;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import ma.znagui.repository.Interface.ClientRepositoryInterface;
 import main.java.ma.znagui.Config.DbConnection;
@@ -28,19 +30,85 @@ public class ClientRepository implements ClientRepositoryInterface {
                int id = rs.getInt(1);
                client.setId(id);
            }
-
            return client;
-
-
-
-
-
-
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
+    }
+
+    public List<Client> getClients() {
+        PreparedStatement ps = null;
+        ResultSet rs  = null;
+        List<Client> clients = new ArrayList<Client>();
+        try{
+            String sql = "select * from clients";
+            ps = conn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                Client client = new Client(rs.getInt("id"),rs.getString("name"),rs.getString("email"),rs.getString("phone"),rs.getBoolean("ispro"));
+                clients.add(client);
+            }
+            return clients;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Client getClient(int id) {
+        PreparedStatement ps = null;
+        ResultSet rs  = null;
+        try{
+            String sql = "select * from clients where id=?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Client client = new Client(rs.getInt("id"),rs.getString("name"),rs.getString("email"),rs.getString("phone"),rs.getBoolean("ispro"));
+                return client;
+            }else {
+                return null;
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean UpdateClient(Client oldClient, Client client) {
+        PreparedStatement ps = null;
+        ResultSet rs  = null;
+        try {
+            String sql = "update clients set name = ?,email = ?,phone = ?,ispro = ? where id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, client.getName());
+            ps.setString(2, client.getEmail());
+            ps.setString(3, client.getPhone());
+            ps.setBoolean(4, client.getIsPro());
+            ps.setInt(5, oldClient.getId());
+            ps.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public boolean deleteClient(Client client) {
+        PreparedStatement ps = null;
+        ResultSet rs  = null;
+        try{
+            String sql = "delete from clients where id = ?";
+            ps = conn.prepareStatement(sql);
+            ps.setInt(1, client.getId());
+            ps.executeUpdate();
+            return true;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
--- App/src/main/java/ma/znagui/repository/Interface/ClientRepositoryInterface.java
+++ App/src/main/java/ma/znagui/repository/Interface/ClientRepositoryInterface.java
@@ -1,6 +1,12 @@
 package ma.znagui.repository.Interface;
 import main.java.ma.znagui.Model.Client;
 
+import java.util.List;
+
 public interface ClientRepositoryInterface {
     public Client addClient(Client client);
+    public Client getClient(int id);
+    public boolean UpdateClient(Client oldClient, Client client);
+    public List<Client> getClients();
+    public boolean deleteClient(Client client);
 }
